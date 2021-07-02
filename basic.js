b16ec1266093070625cbdaa4789edbd1fa4db491
Anmicius <anmicius@Anmiciuss-MacBook-Air.local>
2021-07-02 21:52:12 +0800

    initial commit

---
+++ basic.js
@@ -0,0 +1,69 @@
+module.exports = {
+  record: async (pgClient, message) => {
+    // Check
+    // Check time
+    const date = new Date()
+    if (date.getUTCHours() === 7) {
+      if (date.getUTCMinutes() >= 0 && date.getUTCMinutes() <= 30) {
+        // Invalid
+        message.react("🚫")
+        message.channel.send(
+          "🚫 Guesses between 14:00 to 14:30 are not allowed."
+        )
+      }
+    }
+
+    // Add
+    // Check if there is previous prediction
+    const guild = message.channel.id
+    const user = message.author.id
+    const cases = parseInt(message.content)
+    const name = message.author.username
+
+    let query = await pgClient.query(
+      `SELECT * FROM "prediction" WHERE "guild"=$1 AND "user"=$2 AND "date"=$3`,
+      [guild, user, date]
+    )
+
+    // If there is one record, update it
+    if (query.rows.length === 1) {
+      await pgClient.query(`UPDATE "prediction" SET "cases"=$1 WHERE "id"=$2`, [
+        cases,
+        query.rows[0].id,
+      ])
+    }
+    // Else, insert the record
+    else {
+      await pgClient.query(
+        `INSERT INTO "prediction" ("guild", "user", "name", "cases", "date")
+          VALUES ($1, $2, $3, $4, $5)`,
+        [guild, user, name, cases, date]
+      )
+    }
+  },
+
+  respond: async (pgClient, message) => {
+    // Validate the prediction
+    message.react("✅")
+
+    // Show the board
+    const date = new Date()
+    const guild = message.channel.id
+    const { rows } = await pgClient.query(
+      `SELECT * FROM "prediction" WHERE "guild"=$1 AND "date"=$2 ORDER BY "name" ASC`,
+      [guild, date]
+    )
+
+    let content = `js\n明日確診人數\n`
+    rows.forEach(
+      (row) =>
+        (content += `${row.name}  ${row.cases.slice(
+          0,
+          row.cases.length - 3
+        )}\n`)
+    )
+    console.log(content)
+
+    message.channel.send(`\`\`\`${content}\`\`\``)
+  },
+}
---
+++ main.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+
+// connect Discord
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+// connect database
+const pg = require("pg")
+const pgClient = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+pgClient.connect()
+
+// Import my own functions
+const { record, respond } = require("./basic")
+
+// On ready
+client.on("ready", async () => {
+  console.log("Hello, world")
+})
+
+// On message
+client.on("message", async (message) => {
+  // If it's a pure integer
+  const original = message.content
+  const parsed = parseInt(message.content)
+  if (original == parsed && parsed >= 0) {
+    // Record to DB
+    await record(pgClient, message)
+
+    // Respond
+    respond(pgClient, message)
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
